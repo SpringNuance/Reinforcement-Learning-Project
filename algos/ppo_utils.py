cdf26e661d99bc9584afb1ec32882c9052b2c70d
Nguyen Xuan Binh <xuanbinh.dev@gmail.com>
2023-12-18 15:01:08 +0200

    latest from Binh

--- algos/ppo_utils.py
+++ algos/ppo_utils.py
@@ -23,34 +23,12 @@ class Policy(torch.nn.Module):
         self.fc2_actor = layer_init(torch.nn.Linear(hidden_size, hidden_size))
         self.fc3_actor = layer_init(torch.nn.Linear(hidden_size, action_space))
 
-        # self.actor = nn.Sequential(
-        #                 nn.Linear(state_space, hidden_size),
-        #                 nn.ReLU(),
-        #                 nn.Linear(hidden_size, hidden_size),
-        #                 nn.ReLU(),
-        #                 nn.Linear(hidden_size, action_space),
-        #                 nn.Tanh()
-        #             )
-        
-        # Learnable log standard deviation
-        # self.actor_logstd = nn.Parameter(torch.zeros(1, action_space))
-
         # Fixed log standard deviation, not a learnable parameter
         self.actor_logstd = torch.full((1, action_space), fill_value=0)
 
         self.fc1_critic = layer_init(torch.nn.Linear(state_space, hidden_size))
         self.fc2_critic = layer_init(torch.nn.Linear(hidden_size, hidden_size))
         self.fc3_critic = layer_init(torch.nn.Linear(hidden_size, 1))
-
-        # self.critic = nn.Sequential(
-        #                 nn.Linear(state_space, hidden_size),
-        #                 nn.ReLU(),
-        #                 nn.Linear(hidden_size, hidden_size),
-        #                 nn.ReLU(),
-        #                 nn.Linear(hidden_size, 1)
-        #             )
-        
-        # self.init_weights()
 
     def init_weights(self):
         for m in self.modules():
@@ -64,22 +42,10 @@ class Policy(torch.nn.Module):
         x_actor = self.fc2_actor(x_actor)
         x_actor = F.relu(x_actor)
         x_actor = self.fc3_actor(x_actor)
-        # x_actor = F.tanh(x_actor)
-
-        # print(x_actor.shape)
-        # print(x_actor)
 
         action_mean = self.max_action * F.tanh(x_actor)
-        # action_mean = self.max_action * x_actor
-        # action_mean = x_actor
-        #print(action_mean.shape)
-        # Standard deviation from logstd parameter
-        #action_logstd = self.actor_logstd.expand_as(action_mean)
         action_std = torch.exp(self.actor_logstd)
-        # action_logstd = self.actor_logstd.expand_as(action_mean)
-        # Parameterize the normal distribution
         action_dist = Normal(action_mean, action_std)
-        # Treat each action dimension as independent
         action_dist = Independent(action_dist, 1)
 
         x_critic = self.fc1_critic(x)
@@ -98,132 +64,5 @@ class Policy(torch.nn.Module):
         A higher ratio means more exploration, while a lower ratio means more exploitation.
         """
         self.actor_logstd = torch.full((1, self.action_space), fill_value=ratio_of_episodes)
-
-# # Layer initialization utility
-# def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
-#     torch.nn.init.orthogonal_(layer.weight, std)
-#     torch.nn.init.constant_(layer.bias, bias_const)
-#     return layer
-
-# class Policy(nn.Module):
-#     def __init__(self, state_dim, action_dim, hidden_size=32):
-#         super(Policy, self).__init__()
-#         # Actor network for mean of actions
-#         self.actor_mean = nn.Sequential(
-#             layer_init(nn.Linear(state_dim, hidden_size)), nn.Tanh(),
-#             layer_init(nn.Linear(hidden_size, hidden_size)), nn.Tanh(),
-#             layer_init(nn.Linear(hidden_size, action_dim), std=0.01)
-#         )
-#         # Learnable log standard deviation
-#         self.actor_logstd = nn.Parameter(torch.zeros(1, action_dim))
-
-#         # Critic network for value function
-#         self.critic = nn.Sequential(
-#             layer_init(nn.Linear(state_dim, hidden_size)), nn.Tanh(),
-#             layer_init(nn.Linear(hidden_size, hidden_size)), nn.Tanh(),
-#             layer_init(nn.Linear(hidden_size, 1), std=1)
-#         )
-
-#     def forward(self, state):
-#         # Get mean from actor network
-#         action_mean = self.actor_mean(state)
-        
-#         # Standard deviation from logstd parameter
-#         action_logstd = self.actor_logstd.expand_as(action_mean)
-#         action_std = torch.exp(action_logstd)
-
-#         # Parameterize the normal distribution
-#         action_dist = Normal(action_mean, action_std)
-#         # Treat each action dimension as independent
-#         action_dist = Independent(action_dist, 1)
-
-#         # Get state value from critic network
-#         state_value = self.critic(state)
-
-#         return action_dist, state_value
-
-
-# # Actor-critic agent
-# class Policy(nn.Module):
-#     def __init__(self, state_dim, action_dim, max_action):
-#         super().__init__()
-#         self.max_action = max_action
-#         self.actor = nn.Sequential(
-#             nn.Linear(state_dim, 32), nn.ReLU(),
-#             nn.Linear(32, 32), nn.ReLU(),
-#             nn.Linear(32, action_dim), nn.Tanh()
-#         )
-
-#     def forward(self, state):
-#         return self.max_action * torch.tanh(self.actor(state))
-
-
-# # Critic class. The critic is represented by a neural network.
-# class Critic(nn.Module):
-#     def __init__(self, state_dim, action_dim):
-#         super().__init__()
-#         self.value = nn.Sequential(
-#             nn.Linear(state_dim+action_dim, 32), nn.ReLU(),
-#             nn.Linear(32, 32), nn.ReLU(),
-#             nn.Linear(32, 1))
-
-#     def forward(self, state, action):
-#         x = torch.cat([state, action], 1)
-#         return self.value(x) # output shape [batch, 1]
-    
-
-# class Policy(torch.nn.Module):
-#     def __init__(self, state_space, action_space, hidden_size=32):
-#         super(Policy, self).__init__()
-#         self.fc1_actor = torch.nn.Linear(state_space, hidden_size)
-#         self.fc2_actor = torch.nn.Linear(hidden_size, hidden_size)
-#         self.fc3_actor = torch.nn.Linear(hidden_size, action_space)
-#         self.init_weights()
-
-#     def init_weights(self):
-#         for m in self.modules():
-#             if type(m) is torch.nn.Linear:
-#                 torch.nn.init.normal_(m.weight, 0, 1e-1)
-#                 torch.nn.init.zeros_(m.bias)
-
-#     def forward(self, x):
-#         x_actor = self.fc1_actor(x)
-#         x_actor = F.relu(x_actor)
-#         x_actor = self.fc2_actor(x_actor)
-#         x_actor = F.relu(x_actor)
-#         x_actor = self.fc3_actor(x_actor)
-
-#         # mean = self.fc3(x)
-#         # std_dev = torch.ones_like(mean)  # Fixed standard deviation
-#         # return Normal(mean, std_dev)
-#         return 
-
-
-# class Critic(torch.nn.Module):
-#     def __init__(self, state_space, hidden_size=32):
-#         super(Critic, self).__init__()
-#         self.fc1_critic = torch.nn.Linear(state_space, hidden_size)
-#         self.fc2_critic = torch.nn.Linear(hidden_size, hidden_size)
-#         self.fc3_critic = torch.nn.Linear(hidden_size, 1)
-#         self.init_weights()
-
-#     def init_weights(self):
-#         for m in self.modules():
-#             if type(m) is torch.nn.Linear:
-#                 torch.nn.init.normal_(m.weight, 0, 1e-1)
-#                 torch.nn.init.zeros_(m.bias)
-
-#     def forward(self, x):
-#         x_critic = self.fc1_critic(x)
-#         x_critic = F.relu(x_critic)
-#         x_critic = self.fc2_critic(x_critic)
-#         x_critic = F.relu(x_critic)
-#         x_critic = self.fc3_critic(x_critic)
-#         return x_critic
-
-
-
-
-
 
 
